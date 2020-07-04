49b72350a40acd131bba96343a488ae5ffc2714d
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-07-04 15:55:45 +0300

    feedbacks desktop

--- js/common.js
+++ js/common.js
@@ -1,5 +1,31 @@
 $(function() {
 
-	// Custom JS
+
+	let feedbackSlider = $('.feedback__content');
+
+
+	feedbackSlider.on('afterChange', function(event, slick, currentSlide) {
+
+	  if (currentSlide == 0) {
+	    $(".feedback-carousel-current").text(currentSlide + 1);
+	  } else {
+	    $(".feedback-carousel-current").text(currentSlide + 1);
+	  }
+
+	});
+
+
+
+	feedbackSlider.slick({
+		slidesToShow: 3,
+		centerMode: true,
+		nextArrow: $('.feedback-carousel-next'),
+		prevArrow: $('.feedback-carousel-prev'),
+		
+	});
+
+	$('.feedback-carousel-full').text(feedbackSlider.slick("getSlick").slideCount);
+
+
 
 });
